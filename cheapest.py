1731a4232fc026226e0b5e182c0951fec55a767a
Samik <ohnoalieninvasion@gmail.com>
2025-07-12 11:16:30 -0700

    fixed selection error w/ the core reactor ii ve

--- cheapest.py
+++ cheapest.py
@@ -59,7 +59,7 @@ def find_cheapest_for_price(desired_wattage):
         writer = csv.DictWriter(f, fieldnames=cheapests[0].keys())
         writer.writeheader()
         writer.writerows(cheapests)
-alone = False
+alone = True
 if alone:
     try:
         w = int(input("🔧 Enter the **minimum** PSU wattage (e.g., 650): ").strip())

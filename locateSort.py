18d3469f6656d4c6ae03f4d12411bc054a778389
Samik <ohnoalieninvasion@gmail.com>
2025-09-02 16:58:06 -0700

    unified count for global regions

--- locateSort.py
+++ locateSort.py
@@ -10,11 +10,11 @@ def locateAndSort(region, n):
     sortRegion(region)
     count += (n-1)*5
 
-locateAndSort("",5)
+# locateAndSort("",5)
 locateAndSort("au",3)
-locateAndSort("de",4)
+locateAndSort("de",3)
 locateAndSort("uk",3)
-locateAndSort("ca",4)
+locateAndSort("ca",3)
 
 
 end = time.time()

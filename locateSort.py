79bb9348ecf85c809dc8c772d3d5465078f41093
Samik <ohnoalieninvasion@gmail.com>
2025-11-24 16:08:00 -0800

    data update

--- locateSort.py
+++ locateSort.py
@@ -10,7 +10,7 @@ def locateAndSort(region, n):
     sortRegion(region)
     count += (n-1)*5
 
-locateAndSort("",6)
+locateAndSort("",7)
 locateAndSort("au",3)
 locateAndSort("de",4)
 locateAndSort("uk",4)
